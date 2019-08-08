012c8d291d6ef39f79325cbd5bc8542640a1b15e
juanesgutiz19 <juanesguti19@gmail.com>
2019-08-08 15:14:02 -0500

    Primer cambio

--- src/practicaunoteoria/Main.java
+++ src/practicaunoteoria/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         PrincipalView p = new PrincipalView();
         p.setVisible(true);
+        System.out.println("Jeje");
     }
     
 }
